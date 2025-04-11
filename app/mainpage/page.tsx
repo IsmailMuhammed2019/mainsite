64acaa182387d6ca34c43812fbfd82fc9b7751d1
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2025-04-12 00:53:10 +0100

    update cursor-pointer for buttons

--- app/mainpage/page.tsx
+++ app/mainpage/page.tsx
@@ -462,7 +462,7 @@ const MainPage = () => {
                     </div>
                     <Accordion type="single" collapsible className="grid grid-cols-1 sm:grid-cols-2 gap-6">
                         <AccordionItem value="item-1">
-                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
+                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] cursor-pointer bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
                                 What is Artificial Intelligence (AI)?
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -480,7 +480,7 @@ const MainPage = () => {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-2">
-                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
+                            <AccordionTrigger className="text-sm font-medium cursor-pointer text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
                                 How can AI & Big Data Solutions benefit my business?
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -498,7 +498,7 @@ const MainPage = () => {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-3">
-                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
+                            <AccordionTrigger className="text-sm cursor-pointer font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
                                 What is machine learning?
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -516,7 +516,7 @@ const MainPage = () => {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-4">
-                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
+                            <AccordionTrigger className="text-sm cursor-pointer font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
                                 How does machine learning improve decision-making?
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -534,7 +534,7 @@ const MainPage = () => {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-5">
-                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
+                            <AccordionTrigger className="text-sm cursor-pointer font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
                                 What is big data?
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -552,7 +552,7 @@ const MainPage = () => {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-6">
-                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
+                            <AccordionTrigger className="text-sm cursor-pointer font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
                                 How can big data analysis help my business?
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -570,7 +570,7 @@ const MainPage = () => {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-7">
-                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
+                            <AccordionTrigger className="text-sm cursor-pointer font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
                                 What are the security implications of using AI and big data?
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -588,7 +588,7 @@ const MainPage = () => {
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-8">
-                            <AccordionTrigger className="text-sm font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
+                            <AccordionTrigger className="text-sm cursor-pointer font-medium text-[#4C368C] bg-gray-100 rounded-lg px-6 py-4 border-0 flex justify-between items-center max-w-[500px] min-h-[80px]">
                                 How do I get started with AI, machine learning, and big data solutions?
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -608,38 +608,58 @@ const MainPage = () => {
                     </Accordion>
                 </section>
                 <section className="px-6 lg:px-12 py-12 bg-[#1F1F1F] text-white mt-8 rounded-lg shadow-lg">
-    <div className="flex flex-col lg:flex-row items-center lg:items-start gap-8">
-        {/* Left Side */}
-        <div className="w-full lg:w-1/3 flex justify-center">
-            <img
-                src="./imgs/newsletter.png"
-                alt="Newsletter"
-                className="w-full h-auto max-w-[300px] lg:max-w-[350px] rounded-lg"
-            />
-        </div>
+                    <div className="flex flex-col lg:flex-row items-center lg:items-start gap-8">
+                        {/* Left Side */}
+                        <div className="w-full lg:w-1/3 flex justify-center">
+                            <img
+                                src="./imgs/newsletter.png"
+                                alt="Newsletter"
+                                className="w-full h-auto max-w-[300px] lg:max-w-[350px] rounded-lg"
+                            />
+                        </div>
 
-        {/* Right Side */}
-        <div className="w-full lg:w-2/3 flex flex-col items-start lg:px-8">
-    <p className="text-sm font-bold text-[#4C368C] mb-10 px-8 py-2 bg-white rounded-full">Newsletter</p>
-    <h2 className="text-2xl lg:text-4xl font-bold mb-8">Let's Subscribe the Newsletter</h2>
-    <div className="w-full bg-white rounded-full flex items-center px-4 py-2 mb-4">
-        <input
-            type="email"
-            placeholder="Enter Email"
-            className="flex-grow text-sm text-[#4C368C] bg-transparent outline-none border-none pl-4 font-bold"
-        />
-        <img
-            src="./imgs/send.svg"
-            alt="Send Icon"
-            className="h-6 w-6 cursor-pointer"
-        />
-    </div>
-    <p className="text-sm text-gray-300">
-        Subscribe to our newsletter for exclusive insights, expert tips, and industry news delivered straight to your inbox.
-    </p>
-</div>
-    </div>
-</section>
+                        {/* Right Side */}
+                        <div className="w-full lg:w-2/3 flex flex-col items-start lg:px-8">
+                            <p className="text-sm font-bold text-[#4C368C] mb-10 px-8 py-2 bg-white rounded-full">Newsletter</p>
+                            <h2 className="text-2xl lg:text-4xl font-bold mb-8">Let's Subscribe the Newsletter</h2>
+                            <div className="w-full bg-white rounded-full flex items-center px-4 py-2 mb-4">
+                                <input
+                                    type="email"
+                                    placeholder="Enter Email"
+                                    className="flex-grow text-sm text-[#4C368C] bg-transparent outline-none border-none pl-4 font-bold"
+                                />
+                                <img
+                                    src="./imgs/send.svg"
+                                    alt="Send Icon"
+                                    className="h-6 w-6 cursor-pointer"
+                                />
+                            </div>
+                            <p className="text-sm text-gray-300">
+                                Subscribe to our newsletter for exclusive insights, expert tips, and industry news delivered straight to your inbox.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+                <section className="px-6 lg:px-12 py-12 bg-white mt-8">
+                    <div className="flex flex-col items-center text-center">
+                        {/* Heading */}
+                        <h2 className="text-3xl lg:text-5xl font-bold text-[#4C368C] mb-6">Wish to get in touch?</h2>
+                        {/* Subtext */}
+                        <p className="text-sm lg:text-base text-gray-600 mb-8">
+                            Kindly reach out to us for any enquiries, partnership, or more.
+                        </p>
+                        {/* Button */}
+                        <button className="bg-green-500 text-white px-6 py-3 rounded-full font-semibold cursor-pointer transition duration-300 hover:bg-green-600 mb-6">
+                            Contact Us
+                        </button>
+                        {/* Image */}
+                        <img
+                            src="./imgs/contact.png"
+                            alt="Contact Us"
+                            className="w-full max-w-[300px] lg:max-w-full mt-8"
+                        />
+                    </div>
+                </section>
             </div>
         </div>
     );
